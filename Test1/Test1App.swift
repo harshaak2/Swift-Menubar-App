83ab1e604e0b918ea1a55f28de142662710991aa
harshak2 <sriharshakurra15@gmail.com>
2025-05-17 00:46:36 +0530

    Initial Commit

---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Sri Harsha on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test1Tests/Test1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Test1Tests.swift
+//  Test1Tests
+//
+//  Created by Sri Harsha on 17/05/25.
+//
+
+import Testing
+@testable import Test1
+
+struct Test1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
